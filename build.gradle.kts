6cf46a0eb17d8e396b5b202b4ca017d9fd93a5a8
rfs2671 <156490136+rfs2671@users.noreply.github.com>
2025-10-05 11:04:43 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    plugins {
-        id("com.android.application") version "8.5.2" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    }
+plugins {
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "MudraHub"
-include(":app")
